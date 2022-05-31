d83035be64d5f4647543fc149be7eea61cd0ebde
Tigran <tigranma@gmail.com>
2022-05-31 16:10:28 +0300

    GeoQuiz (MVC) - back button and imageButton added, code update

--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var backButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener{ view: View ->
@@ -46,8 +49,15 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        backButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
     }
+
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
