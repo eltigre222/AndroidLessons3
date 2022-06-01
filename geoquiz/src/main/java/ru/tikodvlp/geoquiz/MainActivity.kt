18fd23f6da13410670197bc63bf3174d462bb77a
Tigran <tigranma@gmail.com>
2022-06-01 17:53:44 +0300

    GeoQuiz (MVC) - land layout added, button enabled after clicking, code update

--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
@@ -2,11 +2,14 @@ package ru.tikodvlp.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -38,23 +42,56 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener{ view: View ->
             checkAnswer(true)
+            trueButton.isClickable = false
+            falseButton.isClickable = false
         }
 
         falseButton.setOnClickListener{ view: View ->
             checkAnswer(false)
+            trueButton.isClickable = false
+            falseButton.isClickable = false
         }
 
         nextButton.setOnClickListener{
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
+            trueButton.isClickable = true
+            falseButton.isClickable = true
         }
 
         backButton.setOnClickListener{
             currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
+            trueButton.isClickable = false
+            falseButton.isClickable = false
         }
 
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 
 
