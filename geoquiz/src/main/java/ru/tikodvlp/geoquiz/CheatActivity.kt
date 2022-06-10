2dfddea8210775cab0feda991795f9504c15bbc6
Tigran <tigranma@gmail.com>
2022-06-10 21:56:41 +0300

    GeoQuiz (MVC) - CheatActivity code update

--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/CheatActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/CheatActivity.kt
@@ -1,5 +1,6 @@
 package ru.tikodvlp.geoquiz
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+const val EXTRA_ANSWER_SHOWN = "ru.tikodvlp.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE = "ru.tikodvlp.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
@@ -28,7 +30,15 @@ class CheatActivity : AppCompatActivity() {
                 else -> R.string.false_button
             }
             answerTextView.setText(answerText)
+            setAnswerShownResult(true)
         }
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
     }
 
     companion object {
--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.tikodvlp.geoquiz
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +16,7 @@ import java.lang.Exception
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -72,10 +74,21 @@ class MainActivity : AppCompatActivity() {
         cheatButton.setOnClickListener{
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
 
         updateQuestion()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.isCheater =
+                data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
     }
 
     override fun onStart() {
@@ -117,10 +130,10 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT,).show()
     }
