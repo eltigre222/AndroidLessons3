c82f1868d6a236e8338fc57a4008c7fba19fe6df
Tigran <tigranma@gmail.com>
2022-07-02 21:57:44 +0300

    CriminalIntent - Change of listItem to Constraint Layout, dateFormat and image visibility update

--- criminalintent/src/main/java/ru/tikodvlp/criminalintent/CrimeListFragment.kt
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/CrimeListFragment.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.text.format.DateFormat
 
 private const val TAG = "CrimeListViewModel"
 
@@ -53,6 +55,7 @@ class CrimeListFragment : Fragment() {
 
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -61,7 +64,13 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            dateTextView.text = DateFormat.format("EEE dd MMM yyyy, hh:mm", this.crime.date)
+            //image visibility
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
