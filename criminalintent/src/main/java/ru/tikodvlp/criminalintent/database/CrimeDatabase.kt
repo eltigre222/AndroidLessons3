48e2754af93703329bcae1a5556b312e401b9aae
Tigran <tigranma@gmail.com>
2022-07-04 22:25:20 +0300

    CriminalIntent - Room database class and type converters added

---
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,12 @@
+package ru.tikodvlp.criminalintent.database
+
+import Crime
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+
+}
