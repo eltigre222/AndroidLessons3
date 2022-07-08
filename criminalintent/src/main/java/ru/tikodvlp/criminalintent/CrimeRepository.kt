3587594bf7d17bc3442d976f37ac67a94154b393
Tigran <tigranma@gmail.com>
2022-07-08 23:26:11 +0300

    CriminalIntent - Room database DAO interface and CrimeRepository added

--- criminalintent/src/main/java/ru/tikodvlp/criminalintent/CrimeRepository.kt
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/CrimeRepository.kt
@@ -1,8 +1,25 @@
 package ru.tikodvlp.criminalintent
 
+import Crime
 import android.content.Context
+import androidx.room.Room
+import ru.tikodvlp.criminalintent.database.CrimeDatabase
+import java.util.*
+
+private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
+
+    private val database : CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
+
+    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
---
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,11 @@
+package ru.tikodvlp.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
