3c8365a28668485260dc5f46136973444a60283e
Tigran <tigranma@gmail.com>
2022-07-11 23:35:44 +0300

    CriminalIntent - Room database DAO interface and CrimeRepository added

--- criminalintent/src/main/java/ru/tikodvlp/criminalintent/database/CrimeDatabase.kt
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/database/CrimeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Crime::class], version = 1)
+@Database(entities = [ Crime::class ], version = 1)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 
