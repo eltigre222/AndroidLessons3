6904dc9ce1086648f9cece5880d1e3a289573f53
Tigran <tigranma@gmail.com>
2022-08-19 22:43:02 +0300

    CriminalIntent - Navigation

--- criminalintent/src/main/java/ru/tikodvlp/criminalintent/database/CrimeDatabase.kt
+++ criminalintent/src/main/java/ru/tikodvlp/criminalintent/database/CrimeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [ Crime::class ], version = 1, exportSchema = false)
+@Database(entities = [ Crime::class ], version = 1)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
