2d355f0db30a04675644fd689caffc6c3de2d3a3
Tigran <tigranma@gmail.com>
2023-04-05 17:51:54 +0300

    Samodelkin app

---
+++ samodelkin/src/main/java/com/example/samodelkin/NewCharacterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.samodelkin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCharacterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_character)
+    }
+}
