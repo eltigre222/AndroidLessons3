4c3dfe714e49f9c5700243dbead157a755ea7c6d
TigranMonte <tigranma@gmail.com>
2023-10-02 21:48:49 +0300

    Lottie Animation

---
+++ lottieapp/src/main/java/com/example/lottieapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.lottieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airbnb.lottie.LottieDrawable
+import com.example.lottieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnDownload.setOnClickListener {
+                lottieView.setMinProgress(0.0f)
+                lottieView.setMaxProgress(0.59f)
+                lottieView.repeatCount = LottieDrawable.INFINITE
+                lottieView.repeatMode = LottieDrawable.REVERSE
+                lottieView.playAnimation()
+            }
+
+            btnStop.setOnClickListener {
+                lottieView.setMinProgress(0.59f)
+                lottieView.setMaxProgress(0.75f)
+                lottieView.repeatCount = 0
+                lottieView.repeatMode = LottieDrawable.RESTART
+                lottieView.playAnimation()
+            }
+
+            btnAnim.setOnClickListener {
+                lottieView.setMinProgress(0.0f)
+                lottieView.setMaxProgress(1.0f)
+                lottieView.repeatCount = 0
+                lottieView.repeatMode = LottieDrawable.RESTART
+                lottieView.playAnimation()
+            }
+        }
+    }
+}
