9764656f2ebbdb115cf73023351151d33874aea0
TigranMonte <tigranma@gmail.com>
2023-10-13 21:46:28 +0300

    Custom View

---
+++ customviewlesson/src/main/java/com/example/customviewlesson/TestView.kt
@@ -0,0 +1,36 @@
+package com.example.customviewlesson
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class TestView(
+    context: Context, attributeSet: AttributeSet
+) : View(context, attributeSet) {
+
+    private val paint = Paint()
+    private val paintC = Paint()
+
+    init {
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.GREEN
+        paint.strokeWidth = 10f
+
+        paintC.style = Paint.Style.FILL
+        paintC.color = Color.GRAY
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        if (canvas != null) {
+            canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintC)
+        }
+        if (canvas != null) {
+            paint.color = Color.RED
+            canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), 100f, paint)
+        }
+    }
+}
