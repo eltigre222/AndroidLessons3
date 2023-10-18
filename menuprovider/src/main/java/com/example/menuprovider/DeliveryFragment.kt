e36bef47726b1114dc5e68525b5e633647cf6600
TigranMonte <tigranma@gmail.com>
2023-10-18 21:52:37 +0300

    MenuProvider

--- menuprovider/src/main/java/com/example/menuprovider/DeliveryFragment.kt
+++ menuprovider/src/main/java/com/example/menuprovider/DeliveryFragment.kt
@@ -11,14 +11,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.MenuProvider
 
-class MainFragment : Fragment(), MenuProvider {
+class DeliveryFragment : Fragment(), MenuProvider {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_delivery, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,26 +28,23 @@ class MainFragment : Fragment(), MenuProvider {
 
     companion object {
         @JvmStatic
-        fun newInstance() =  MainFragment()
+        fun newInstance() = DeliveryFragment()
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-          menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.deliver_menu, menu)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        when (menuItem.itemId) {
-            R.id.chat -> {
-                Toast.makeText(requireContext(), "Chat", Toast.LENGTH_LONG).show()
+        when(menuItem.itemId) {
+            R.id.discount -> {
+                Toast.makeText(requireContext(), "Discount", Toast.LENGTH_LONG).show()
             }
-            R.id.delivery -> {
+            R.id.home -> {
                 requireActivity().supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.host, DeliveryFragment.newInstance())
+                    .replace(R.id.host, MainFragment.newInstance())
                     .commit()
-            }
-            R.id.notification -> {
-                Toast.makeText(requireContext(), "Notification", Toast.LENGTH_LONG).show()
             }
         }
         return true
