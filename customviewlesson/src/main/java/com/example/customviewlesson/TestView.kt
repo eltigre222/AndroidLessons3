358c7c373d1e3409197afb8c8f9f9bb116cdf5da
TigranMonte <tigranma@gmail.com>
2025-01-08 20:20:59 +0300

    upgrade

--- customviewlesson/src/main/java/com/example/customviewlesson/TestView.kt
+++ customviewlesson/src/main/java/com/example/customviewlesson/TestView.kt
@@ -26,10 +26,12 @@ class TestView(
         paintC.style = Paint.Style.FILL
         paintC.color = Color.BLACK
     }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawCircleButton(canvas)
     }
+
     private fun drawCircleButton(canvas: Canvas) {
         val centerX = width / 2f
         val centerY = width / 2f
@@ -48,6 +50,7 @@ class TestView(
             )
         }
     }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val centerX = width / 2f
         val centerY = width / 2f
