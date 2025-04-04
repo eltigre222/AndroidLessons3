a0f64b7c0f9c2759cd4e96121079d72aa70e1f76
TigranMonte <tigranma@gmail.com>
2025-04-04 22:22:51 +0300

    upgrade

--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
@@ -123,14 +123,17 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         Log.d(TAG, "onResume called")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause called")
     }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop called")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy called")
