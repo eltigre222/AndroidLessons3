2a95b4a72d179a2aba0156cde02448db2c930881
TigranMonte <tigranma@gmail.com>
2025-08-22 21:01:00 +0300

    upgrade

--- geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
+++ geoquiz/src/main/java/ru/tikodvlp/geoquiz/MainActivity.kt
@@ -130,18 +130,15 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         Log.d(TAG, "onStop called")
     }
-
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy called")
     }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Log.i(TAG, "onSaveInstanceState")
         outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
-
     private fun updateQuestion() {
         val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
